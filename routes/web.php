f36a55ffb9254fd37efbb5df08d7527e835b9af2
Tomasz Nowacki <tomasz.nowacki@poznan.merito.pl>
2023-10-01 17:56:14 +0200

    przekazanie danych - tablica

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout');
 }); 
 Route::get('/kontakt', function () {
     return view('ogolny.kontakt');
 }); 
 Route::get('/onas', function () {
-    return view('ogolny.onas');
+    $zadania = [
+        'Zadanie 1',
+        'Zadanie 2',
+        'Zadanie 3'
+    ];
+    //return view('ogolny.onas',['zadania' => $zadania]);
+    return view('ogolny.onas')->with(['zadania' => $zadania]);
 }); 
